bda5476b185aacfb7eda7647acee58a614c66ae8
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-08-09 15:22:31 -0700

    first test passing for creating a new user

---
+++ tests/users.test.js
@@ -0,0 +1,35 @@
+const { setupDb } = require('./utils.js');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+  email: 'test@user.com',
+  password: '123456',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  const { email } = user;
+  await (await agent.post('/api/v1/users/sessions')).setEncoding({ email, password });
+  return [agent, user];
+};
+
+describe('users', () => {
+  beforeEach(setupDb);
+
+  it.only('should create a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
+  });
+});
